7530ffedcf2649fea213656190e7be0a42929b55
Michael Leveton <>
2022-09-25 13:06:17 -0400

    first commit

---
+++ DataManager/DataManager/DataManager.h
@@ -0,0 +1,18 @@
+//
+//  DataManager.h
+//  DataManager
+//
+//  Created by Michael Leveton on 9/25/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataManager.
+FOUNDATION_EXPORT double DataManagerVersionNumber;
+
+//! Project version string for DataManager.
+FOUNDATION_EXPORT const unsigned char DataManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataManager/PublicHeader.h>
+
+
---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Michael Leveton on 9/25/22.
+//
+
+import UIKit
+import DataManager
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
