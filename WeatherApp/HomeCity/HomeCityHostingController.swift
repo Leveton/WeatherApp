2b1d86811f2c4528f7e2b5d0b28547801ca618cd
Michael Leveton <>
2022-09-25 22:25:10 -0400

    Home city view controller now the initial view controller

--- WeatherApp/HomeCity/HomeCityHostingController.swift
+++ WeatherApp/HomeCity/HomeCityHostingController.swift
@@ -10,3 +10,19 @@ import SwiftUI
 
 class HomeCityHostingController: UIHostingController<HomeCityView> {
 }
+
+class HomeCityViewController: UIViewController {
+    
+    @IBSegueAction func HomeCityViewControllerToHomeCityView(_ coder: NSCoder) -> UIViewController? {
+        
+        let viewModel = HomeCityViewModel()
+        let homeCityView = HomeCityView(viewModel: viewModel)
+        
+        guard let vc = HomeCityHostingController(coder: coder, rootView: homeCityView) else {
+            return UIViewController()
+        }
+        
+        return vc
+    }
+    
+}
